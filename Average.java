83b6ea38e097c03a0e685b85547dad1206ec6dc5
Anna Krzysik <instalatorka@gmail.com>
2021-11-01 11:48:21 +0100

    Average na tablicach

--- Average.java
+++ Average.java
@@ -5,12 +5,27 @@ public class Average {
         Scanner czytnik = new Scanner(System.in);
         System.out.print("Podaj liczbę ocen: ");
         int licznik = czytnik.nextInt();
-        double suma = 0;
+
+        //uutwórz tablicę o rozmiarze takim jak zadeklarowana liczba ocen
+        double[] oceny = new double[licznik];
         for (int i=0; i<licznik; i++) {
-           suma += wczytajOcene(czytnik);
+            //wypełnij komórkę tablicy o indeksie i oceną wczytaną z wejścia
+           oceny[i]= wczytajOcene(czytnik);
         }
 
-        System.out.println(suma/licznik);
+        System.out.println("Liczba ocen: " + oceny.length);
+        double suma = 0;
+        for (int i=0; i<oceny.length; i++) {
+            //powiększ aktualną sumę o wartość z kolejnej komórki tablicy
+            suma += oceny[i];
+        }
+            
+        //wypisz średnią arytmetyczną
+        System.out.println("Średnia: " + suma/oceny.length);
+        
+
+
+
     }
 
     private static double wczytajOcene(Scanner czytnik) {
