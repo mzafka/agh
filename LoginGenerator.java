f469b50e555a3208b4ba419b00106ebe89dd1626
Anna Krzysik <instalatorka@gmail.com>
2021-11-01 14:08:28 +0100

    LoginGenerator

---
+++ LoginGenerator.java
@@ -0,0 +1,11 @@
+public class LoginGenerator {
+
+    public static void main(String[] args) {
+
+        String[] studentNames = { "Budynek Piotr", 
+                                  "Chalupa Krystyna", 
+                                  "Wiezowiec Jan",
+                                  "Szkieletor Andrzej",
+                                  "Domek Marianna" };
+    }
+}
