f76f95e2593535687b9bcbff1a62cd634f678d45
Anna Krzysik <instalatorka@gmail.com>
2021-11-01 10:43:19 +0100

    triangle - maly trojkat, op. na integerach

---
+++ Triangle.java
@@ -0,0 +1,15 @@
+public class Triangle {
+    public static void main(String[] args) {
+        int a = 5;
+        int h = 3;
+
+        int triangle = a*h/2;
+
+        if (triangle>7) {
+            System.out.println("Duzy trojkat");
+        }
+        else {
+            System.out.println("Maly trojkat");
+        }
+    }
+}
